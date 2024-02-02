b18050e3857f36482f5a7262810cdae11ec6c538
edgar <santiago7nubesoft@gmail.com>
2024-02-01 21:14:49 -0600

    refactor: update value of UMA 2024

--- app/src/main/java/com/mozama/impuestos/fragments/IvaFragment.kt
+++ app/src/main/java/com/mozama/impuestos/fragments/IvaFragment.kt
@@ -293,7 +293,7 @@ class IvaFragment : Fragment() {
     }
 
     private fun calcInputTotal() {
-        if (txtTotal.text.isNotEmpty()){
+        if (txtTotal.text.toString().isNotEmpty()){
             val text = txtTotal.text.toString()
             val subNotComma = UtilsGraphic().deleteComma(text)
             val temp = subNotComma.toDoubleOrNull()
